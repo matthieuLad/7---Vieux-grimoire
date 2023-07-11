f20b6a6a3a96539df557fed375eb6a8c5ff1411d
TurboTruite <ladouce.matthieu@gmail.com>
2023-07-11 15:33:54 +0200

    Sharp compression ok

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,43 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const storage = multer.memoryStorage();
+const upload = multer({storage});
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+// const imageUpload = require('../middleware/sharp-config')
 
-module.exports = multer({storage: storage}).single('image');
+// const MIME_TYPES = {
+//   'image/jpg': 'jpg',
+//   'image/jpeg': 'jpg',
+//   'image/png': 'png'
+// };
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(' ').join('_');
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + '.' + extension);
+//   }
+// });
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(' ').join('_');
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + '.' + extension);
+//   }
+// });
+
+module.exports = upload.single('image');
+
+// module.exports = function(app) {
+//   app.post("/", uploadFile.single('image'), async (request, response) => {
+//     await imageUpload(request);
+//   });
+// }
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const booksCtrl = require('../controllers/books');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
+const sharp = require('../middleware/sharp-config')
 
 router.get('/', booksCtrl.getAllBooks);
-router.post('/', auth, multer, booksCtrl.createBook);
+router.post('/', auth, multer, sharp.imageUpload, booksCtrl.createBook);
 router.get('/:id', booksCtrl.getOneBook);
 router.put('/:id', auth, multer, booksCtrl.modifyBook);
 router.delete('/:id', auth, booksCtrl.deleteBook);
