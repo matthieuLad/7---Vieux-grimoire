5117f8fdc80b9c118bec41d4f9f6743209a569ae
TurboTruite <ladouce.matthieu@gmail.com>
2023-07-12 11:34:38 +0200

    Fonction rateBook OK

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -87,5 +87,28 @@ exports.getBestBooks = (req, res, next) => {
 }
 
 exports.rateBook = (req, res, next) => {
-
+    Book.findOne({_id: req.params.id})
+        .then((book) => {
+            let ratingsTotal = 0;
+            for (let i = 0; i < book.ratings.length; i++) {
+                ratingsTotal = ratingsTotal + book.ratings[i].grade
+            };
+            const average = (ratingsTotal + req.body.rating) / (book.ratings.length + 1 );
+            if(!book.ratings.includes(req.auth.userId)) {
+                Book.findOneAndUpdate(
+                    { _id: req.params.id},
+                    {
+                        $push: {ratings: [{userId: req.auth.userId, grade: req.body.rating}]}, 
+                        $set: {"averageRating": average}
+                    }, 
+                    {new:true}
+                    )
+                .then((book) => {
+                    res.status(200).json(book)
+                }
+                )
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch(error => res.status(400).json({ error }))
 }
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,43 +1,5 @@
 const multer = require('multer');
-const sharp = require('sharp');
 const storage = multer.memoryStorage();
 const upload = multer({storage});
 
-
-// const imageUpload = require('../middleware/sharp-config')
-
-// const MIME_TYPES = {
-//   'image/jpg': 'jpg',
-//   'image/jpeg': 'jpg',
-//   'image/png': 'png'
-// };
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, 'images');
-//   },
-//   filename: (req, file, callback) => {
-//     const name = file.originalname.split(' ').join('_');
-//     const extension = MIME_TYPES[file.mimetype];
-//     callback(null, name + Date.now() + '.' + extension);
-//   }
-// });
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, 'images');
-//   },
-//   filename: (req, file, callback) => {
-//     const name = file.originalname.split(' ').join('_');
-//     const extension = MIME_TYPES[file.mimetype];
-//     callback(null, name + Date.now() + '.' + extension);
-//   }
-// });
-
 module.exports = upload.single('image');
-
-// module.exports = function(app) {
-//   app.post("/", uploadFile.single('image'), async (request, response) => {
-//     await imageUpload(request);
-//   });
-// }
