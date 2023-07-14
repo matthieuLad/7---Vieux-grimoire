470f8183be6869b5317890c4a155af39bd0bc462
TurboTruite <ladouce.matthieu@gmail.com>
2023-07-14 15:44:25 +0200

    Prettier

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'CECI_EST_EXTREMEMENT_SECRET');
-        const userId = decodedToken.userId;
-        req.auth = {
-            userId: userId
-        };
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "CECI_EST_EXTREMEMENT_SECRET");
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
     next();
-    } catch(error) {
-        res.status(401).json({ error });
-    }
+  } catch (error) {
+    res.status(401).json({ error });
+  }
 };
